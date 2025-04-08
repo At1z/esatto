2b0759a1edd4d7f8ff3c1f920d1e88929da268ff
At1z <Adrianlistkiewiczv2@gmail.com>
2025-04-08 17:21:44 +0200

    cors

--- esatto/src/main/java/com/atis/esatto/controller/APIController.java
+++ esatto/src/main/java/com/atis/esatto/controller/APIController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/currency")
+@CrossOrigin(origins = "http://localhost:5173")
 public class APIController {
 
     @Autowired
